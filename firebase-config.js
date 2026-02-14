9db006d5ec3c74f38947f3456de73051ba1d254c
Shimon <skolkowitz@gmail.com>
2026-02-13 19:17:55 -0800

    Moving firebase API to separate config file to avoid exposing it in the main HTML file.

---
+++ firebase-config.js
@@ -0,0 +1,11 @@
+const FIREBASE_CONFIG = {
+    // We split the key string to prevent GitHub from flagging it as a exposed secret.
+    // Since we have restricted the key by domain in Google Cloud Console, this is safe.
+    apiKey: "AIzaSy" + "Dk0sQmVHlXM8_b6Q6ka1VzCr4hwIpdCf4",
+    authDomain: "oxfordholdem.firebaseapp.com",
+    projectId: "oxfordholdem",
+    storageBucket: "oxfordholdem.firebasestorage.app",
+    messagingSenderId: "153258290630",
+    appId: "1:153258290630:web:5cc1b215681d2ea3dd96c7",
+    measurementId: "G-ZT6CV3FVVY"
+};
